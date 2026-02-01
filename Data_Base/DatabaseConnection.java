c0bb9f79cf6a36ddba37bf828a1f56900b356854
ghimirebabin07 <240107babin@cosmoscollege.edu.np>
2026-02-01 10:37:18 +0545

    Modifying the code and asking for the user to enter the  password because of security

--- Data_Base/DatabaseConnection.java
+++ Data_Base/DatabaseConnection.java
@@ -2,6 +2,7 @@ package Data_Base;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.io.Console;
 
 public class DatabaseConnection {
 
@@ -9,20 +10,36 @@ public class DatabaseConnection {
         Connection con = null;
 
         try {
+            Console console = System.console();
+
+            if (console == null) {
+                System.out.println("Run this from terminal, not IDE Run button");
+                return null;
+            }
+
+            String user = console.readLine("Enter DB Username: ");
+            char[] passwordArray = console.readPassword("Enter DB Password: ");
+            String password = new String(passwordArray);
+
             Class.forName("org.postgresql.Driver");
 
             con = DriverManager.getConnection(
                 "jdbc:postgresql://localhost:5432/college_db",
-                "postgres",
-                "Babin123" 
+                user,
+                password
             );
 
-            System.out.println("Database connected successfully");
+            System.out.println(" Database connected successfully");
 
         } catch (Exception e) {
             e.printStackTrace();
         }
 
         return con;
+    }
+
+    // MAIN METHOD (this fixes your error)
+    public static void main(String[] args) {
+        getConnection();
     }
 }
