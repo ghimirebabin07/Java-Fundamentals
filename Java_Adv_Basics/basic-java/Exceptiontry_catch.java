b13c65fa25ab42921e30e80f5bc2515a1b5b8240
ghimirebabin07 <240107babin@cosmoscollege.edu.np>
2026-04-12 20:58:32 +0545

    Removed

--- Java_Adv_Basics/basic-java/Exceptiontry_catch.java
+++ Java_Adv_Basics/basic-java/Exceptiontry_catch.java
@@ -1,13 +1,2 @@
 public class Exceptiontry_catch{
-    public static void main(String args []){
-        try{
-            int a = 10 ;
-            int b = 0;
-            int c =a/b;
-            System.out.println(c);
-
-            }
-            catch(ArithmeticException e){
-            System.out.println("Error:zero can't be divied ");}
-        }
-    }
+   
